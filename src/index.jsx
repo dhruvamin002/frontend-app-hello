999cb2f8b53248ede2a489bc5e54e223b83a3f09
dhruvamin002 <amindhruv3@gmail.com>
2025-11-24 00:15:27 +0530

    from template app

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import {
+  APP_INIT_ERROR, APP_READY, subscribe, initialize,
+} from '@edx/frontend-platform';
+import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
 import ReactDOM from 'react-dom';
-import { initialize } from '@edx/frontend-platform';
 
-function App() {
-  return <h1>Hello from Custom MFE!</h1>;
-}
+import Header from '@edx/frontend-component-header';
+import { FooterSlot } from '@edx/frontend-component-footer';
+import messages from './i18n';
+import ExamplePage from './example/ExamplePage';
 
-initialize().then(() => {
-  ReactDOM.render(<App />, document.getElementById('root'));
+import './index.scss';
+
+subscribe(APP_READY, () => {
+  ReactDOM.render(
+    <AppProvider>
+      <Header />
+      <ExamplePage />
+      <FooterSlot />
+    </AppProvider>,
+    document.getElementById('root'),
+  );
+});
+
+subscribe(APP_INIT_ERROR, (error) => {
+  ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
+});
+
+initialize({
+  messages,
 });
